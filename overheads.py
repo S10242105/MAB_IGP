cf4f912beef8b7cdc791b8889925db1ce6c34384
Deswertim <S10243526@connect.np.edu.sg>
2023-01-31 21:10:07 +0800

    Version 3

--- overheads.py
+++ overheads.py
@@ -1,29 +1,29 @@
 from pathlib import Path
 import csv
 
-def Overhead():
-    fp = Path.cwd()/"csv_reports"/"overheads-day-90.csv"
+def FindMaxOverhead():
+    fp = Path.cwd()/"csv_reports"/"overheads-day-90.csv" 
+   
     with fp.open(mode="r", encoding="UTF-8", newline="") as file:
         reader = csv.reader(file)
-        next(reader)   
-
-        overhead = []   
+        next(reader)    
+        overheadcsv = []   
+    
         for row in reader:
-            overhead.append(row)
-
-    category_base = ''
-    overhead_base = ''
-    for row in overhead:
-        category = row[0]
-        overhead =float(row[1])
-        if overhead_base == '':
-            overhead_base = overhead
-        else:
-            if overhead > overhead_base: 
-                overhead_base = overhead
-                category_base = category
-            elif overhead < overhead_base:
-                overhead_base = overhead_base    
-                category = category_base   
-    print(f'[HIGHEST OVERHEADS] {category}: {overhead_base}%')
-Overhead()
+            overheadcsv.append(row)
+    
+    filepath = Path.cwd()/"summary_report.txt"  
+    with filepath.open(mode = "w", encoding = "UTF-8") as file:    
+        maxOverhead = float(overheadcsv[0][1])  
+        
+        for row in overheadcsv:
+            category = row[0]  
+            overhead = float(row[1])   
+            if overhead > maxOverhead:  
+                maxOverhead = overhead 
+       
+        for row in overheadcsv:
+            category = row[0]   
+            overhead = float(row[1])    
+            if overhead == maxOverhead:
+                file.write(f'[HIGHEST OVERHEADS] {category}: {overhead}%\n')    
