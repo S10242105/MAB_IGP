23df4ae53b16da3b136737c7e26dab9d7bb01ce6
S10242105 <S10242105@connect.np.edu.sg>
2023-01-31 21:47:34 +0800

    Version 2

--- cash_on_hand.py
+++ cash_on_hand.py
@@ -1,29 +1,31 @@
 from pathlib import Path
 import csv
 
-fp = Path.cwd()/"csv_reports"/"cash-on-hand-usd.csv"
-with fp.open(mode="r", encoding="UTF-8", newline="") as file:
-    reader = csv.reader(file)
-    next(reader)
+def CashOnHand():
+    fp = Path.cwd()/"csv_reports"/"cash-on-hand-usd.csv"    
 
-    day = []
-    cash_on_hand = []
+    with fp.open(mode="r", encoding="UTF-8", newline="") as file:
+        reader = csv.reader(file)
+        next(reader)   
+        cash_on_hand = []  
+     
+        for row in reader:
+            cash_on_hand.append(row)
 
-    for row in reader:
-        day.append(row[0])
-        cash_on_hand.append(float(row[1]))
-
-a = 0
-b = 1
-
-for i in cash_on_hand:
-    while b < 11:
-        if cash_on_hand[a] > cash_on_hand[b]:
-            difference = cash_on_hand[a] - cash_on_hand[b]
-            print(f"[CASH DEFICIT] DAY: {day[b]}, AMOUNT: USD{difference}")
-            a += 1
-            b += 1
-        elif cash_on_hand[a] < cash_on_hand[b]:
-            print(f'[CASH SURPLUS] CASH ON DAY {day[b]} IS HIGHER THAN DAY {day[a]}')
-            a += 1
-            b += 1
+    filepath = Path.cwd()/"summary_report.txt" 
+    with filepath.open(mode = "a", encoding = "UTF-8") as file:   
+        prevCOH = ''    
+        cash_deficit = 0   
+      
+        for row in cash_on_hand:
+            day = float(row[0])     
+            cash = float(row[1])    
+            if prevCOH == '':       
+                prevCOH = cash     
+            else:                   
+                if cash < prevCOH:   
+                    file.write(f'[CASH DEFICIT] DAY: {day}, AMOUNT: USD{prevCOH-cash}\n')  
+                    cash_deficit += 1 
+                prevCOH = cash
+        if cash_deficit == 0:      
+            file.write(f'[CASH SURPLUS] CASH ON EACH DAY IS HIGHER THAN THE PREVIOUS DAY')  
