ffe5317b40e014419dea4cc3b639a055dfc0bd2c
Lucaschongtf01 <bobthecarrot72@gmail.com>
2023-01-31 21:50:43 +0800

    main plus comments

--- overheads.py
+++ overheads.py
@@ -2,28 +2,31 @@ from pathlib import Path
 import csv
 
 def FindMaxOverhead():
-    fp = Path.cwd()/"csv_reports"/"overheads-day-90.csv" 
-   
+    """
+    - Function to find the highest overhead category.
+    """
+    fp = Path.cwd()/"project_group"/"csv_reports"/"overheads-day-90.csv"    # Path to csv input file
+    # Read data from csv
     with fp.open(mode="r", encoding="UTF-8", newline="") as file:
         reader = csv.reader(file)
-        next(reader)    
-        overheadcsv = []   
-    
+        next(reader)    # Skip csv header
+        overheadcsv = []    # Initialise variable to store data from csv
+        # Read all data into overheadcsv
         for row in reader:
             overheadcsv.append(row)
     
-    filepath = Path.cwd()/"summary_report.txt"  
-    with filepath.open(mode = "w", encoding = "UTF-8") as file:    
-        maxOverhead = float(overheadcsv[0][1])  
-        
+    filepath = Path.cwd()/"summary_report.txt"  # Path to output file
+    with filepath.open(mode = "w", encoding = "UTF-8") as file:     # Open output file for write
+        maxOverhead = float(overheadcsv[0][1])  # Assign the first overhead to maxOverhead 
+        # Iterate over each row to find the largest overhead
         for row in overheadcsv:
-            category = row[0]  
-            overhead = float(row[1])   
-            if overhead > maxOverhead:  
-                maxOverhead = overhead 
-       
+            category = row[0]   # Category
+            overhead = float(row[1])    # Overhead as a float
+            if overhead > maxOverhead:  # Compares if overhead is larger than stored maxOverhead value
+                maxOverhead = overhead  # Store larger overhead value as maxOverhead
+        # Second iteration, find all accounts with maximum overhead and write to output file
         for row in overheadcsv:
             category = row[0]   
             overhead = float(row[1])    
-            if overhead == maxOverhead:
-                file.write(f'[HIGHEST OVERHEADS] {category}: {overhead}%\n')    
+            if overhead == maxOverhead: # Checks if the overhead value is the highest
+                file.write(f'[HIGHEST OVERHEADS] {category}: {overhead}%\n')    # Write in output file
--- profit_loss.py
+++ profit_loss.py
@@ -2,30 +2,33 @@ from pathlib import Path
 import csv
 
 def ProfitLoss():
-    fp = Path.cwd()/"project_group"/"csv_reports"/"profit-and-loss-usd.csv"     
-
+    """
+    - Function calculates the difference in Net Profit if the net profit on the current day is lower than the previous.
+    """
+    fp = Path.cwd()/"project_group"/"csv_reports"/"profit-and-loss-usd.csv"     # Path to csv input file
+    # Read data from csv
     with fp.open(mode="r", encoding="UTF-8", newline="") as file:
         reader = csv.reader(file)
-        next(reader) 
-        profit_loss = []   
-        
+        next(reader)    # Skip csv header
+        profit_loss = []    # Initialise to store data from csv
+        # Read all data into profit_loss
         for row in reader:
             profit_loss.append(row)
 
-    filepath = Path.cwd()/"summary_report.txt"  
-    with filepath.open(mode = "a", encoding = "UTF-8") as file:   
-        prevPL = ''   
-        profit_deficit = 0    
-
+    filepath = Path.cwd()/"summary_report.txt"  # Path to output file
+    with filepath.open(mode = "a", encoding = "UTF-8") as file:     # Append into output file
+        prevPL = ''     # Store previous net profit for comparison. Initialise as empty string.
+        profit_deficit = 0      # Stores total number of net profit deficits
+        # Iterate over each row
         for row in profit_loss:
-            day = float(row[0])    
-            net_profit = float(row[4])  
-            if prevPL == '':            
-                prevPL = net_profit    
-            else:                     
-                if net_profit < prevPL:    
-                    file.write(f'[NET PROFIT DEFICIT] DAY: {day}, AMOUNT: USD{prevPL-net_profit}\n')    
-                    profit_deficit += 1    
+            day = float(row[0])     # Day as a float
+            net_profit = float(row[4])  # net profit as a float
+            if prevPL == '':            # prevPL does not yet contain previous net profit
+                prevPL = net_profit     # Store first net profit into prevPL for comparison
+            else:                       # prevPL already contains previous net profit
+                if net_profit < prevPL:     # Checks if current day net profit is lower than previous
+                    file.write(f'[NET PROFIT DEFICIT] DAY: {day}, AMOUNT: USD{prevPL-net_profit}\n')    # Write in output file
+                    profit_deficit += 1     # Increment profit_deficit counter
                 prevPL = net_profit
-        if profit_deficit == 0:     
-            file.write(f'[NET PROFIT SURPLUS] NET PROFIT ON EACH DAY IS HIGHER THAN THE PREVIOUS DAY')  
+        if profit_deficit == 0:     # If no profit_deficit, then it is all surplus (or same)
+            file.write(f'[NET PROFIT SURPLUS] NET PROFIT ON EACH DAY IS HIGHER THAN THE PREVIOUS DAY')  # Write in output file
