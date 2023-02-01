7b1baa58afd5fd0e0daf7d7cad7ebd28f0628399
S10242105 <S10242105@connect.np.edu.sg>
2023-02-01 13:28:58 +0800

    Final Commit

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import cash_on_hand, profit_loss, overheads
 
 def main():
-
+    """
+    - Main function which coordinates and executes the functions FindMaxOverhead, CashOnHand and ProfitLoss
+    """
     overheads.FindMaxOverhead()
     cash_on_hand.CashOnHand()
     profit_loss.ProfitLoss()
